7715de940845d5ddbb23f0b4b90d9cd740cdbddd
Artur Vartsaba <arturvartsaba1@gmail.com>
2026-01-13 14:56:23 +0200

    õpilase otsing - funktsioon

--- funktsioonid.php
+++ funktsioonid.php
@@ -1,17 +1,21 @@
 <?php
 //Uudiste lugemise funktsioon
+
+//pildid
+//$imgurl = $item -> enclosure -> attributes() -> url;
+//            echo "<img src='$imgurl'>";
 function uudised($url, $kogus)
 {
     $feed=simplexml_load_file($url);
     echo "<ul>";
-    echo "kuupäev: ".date("d.m.Y", $feed->channel->item->pubDate);
+    echo "kuupäev: ".date("d.m.Y", strtotime($feed->channel->item->pubDate));
     $loendur=0;
     foreach ($feed->channel->item as $item){
         if($loendur<=$kogus){
             echo "<li>";
             echo "<a href='$item->link' target='_blank'>".$item->title."</a>";
             echo $item->description;
-            echo "<img src='$item->image'>";
+            echo "<img src='$feed->channel->image->url' alt=''>";
             echo "</li>";
             $loendur++;
         }
