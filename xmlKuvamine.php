18ac43b6753c8baa1fec4244ce770aa5512acb5b
Artur Vartsaba <arturvartsaba1@gmail.com>
2026-01-16 10:48:34 +0200

    on loodud õpilase lisamine

--- xmlKuvamine.php
+++ xmlKuvamine.php
@@ -1,5 +1,37 @@
 <?php
+function lisaOpilane()
+{
+    $xmlDoc = new DOMDocument("1.0", "UTF-8");
+    $xmlDoc->preserveWhiteSpace = false;
+    $xmlDoc->load("opilased.xml");
+    $xmlDoc->formatOutput = true;
+
+    $xmlOpilane = $xmlDoc->createElement("opilane");
+    $xmlDoc->appendChild($xmlOpilane);
+
+    $xmlRoot = $xmlDoc->documentElement;
+    $xmlRoot->appendChild($xmlOpilane);
+
+    $elukoht = $xmlDoc->createElement("elukoht");
+    $xmlOpilane->appendChild($elukoht);
+    unset($_POST["submit"]);
+
+    foreach ($_POST as $voti => $vaartus)
+    {
+        $kirje = $xmlDoc->createElement($voti, $vaartus);
+
+        if ($voti == "linn" || $voti == "maakond")
+            $elukoht->appendChild($kirje);
+        else
+            $xmlOpilane->appendChild($kirje);
+    }
+
+    $xmlDoc->save("opilased.xml");
+    header("Refresh:0");
+}
+
 $opilased=simplexml_load_file("opilased.xml");
+
 //õpilase otsing
 function erialaOtsing($paring)
 {
@@ -27,6 +59,7 @@ function erialaOtsing($paring)
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="tableStyle.css">
     <title>XML faili kuvamine - Opilased.xml</title>
 </head>
 <body>
@@ -45,11 +78,27 @@ echo "1.õpilase nimi: ".$opilased->opilane[0]->nimi;
 if(!empty($_POST['otsing']))
 {
     $tulemus=erialaOtsing($_POST['otsing']);
-    foreach($tulemus as $opilane)
-    {
-        echo $opilane->nimi." - ".$opilane->eriala."<br>";
+
+        echo " <table>
+    <tr>
+        <th>Õpilase nimi</th>
+        <th>isikukood</th>
+        <th>Eriala</th>
+        <th>Elukoht</th>
+    </tr>";
+
+    foreach($tulemus as $opilane){
+        echo "<tr>";
+        echo "<td>".$opilane->nimi."</td>";
+        echo "<td>".$opilane->isikukood."</td>";
+        echo "<td>".$opilane->eriala."</td>";
+        echo "<td>".$opilane->elukoht->linn."</td>", ".
+        $opilane->elukoht->maakond.</th>";
+        echo "</tr>";
     }
-}
+    echo "</table>";
+
+} else {
 ?>
 <table>
     <tr>
@@ -68,7 +117,46 @@ if(!empty($_POST['otsing']))
         $opilane->elukoht->maakond.</th>";
         echo "</tr>";
     }
+    }
     ?>
 </table>
+
+<h2>Õpilase sisestamine</h2>
+
+<form>
+    <tr>
+        <td><label for="nimi">Nimi:</label></td>
+        <td><input type="text" id="nimi" name="nimi"></td>
+    </tr>
+    <br>
+    <tr>
+        <td><label for="isikukood">Isikukood:</label></td>
+        <td><input type="text" id="isikukood" name="isikukood"></td>
+    </tr>
+    <br>
+    <tr>
+        <td><label for="eriala">Eriala:</label></td>
+        <td><input type="text" id="eriala" name="eriala"></td>
+    </tr>
+    <br>
+    <tr>
+        <td><label for="elukoht">Elukoht:</label></td>
+        <td><input type="text" id="elukoht" name="elukoht"></td>
+    </tr>
+    <br>
+    <tr>
+        <td><input type="submit" id="submit" name="submit" value="Sisesta"></td>
+        <td></td>
+    </tr>
+</form>
+
+<?php
+    if(isset($_POST['submit']))
+    {
+       lisaOpilane();
+       echo "<p>Õpilane on lisatud!</p>";
+    }
+?>
+
 </body>
 </html>
